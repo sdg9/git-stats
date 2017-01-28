0a0761380560d07ae893feb5ec194fa11ab5a7b7
Steven Goff <stevengoff@discover.com>
2017-01-28 10:41:12 -0600

    Enhances logic around issue/PR data

--- example/index.js
+++ example/index.js
@@ -4,6 +4,9 @@ var GitStats = require("../lib");
 // Create the GitStats instance
 var g1 = new GitStats();
 g1.initConfig();
-g1.getPullRequests({}, function (err, data) {
+// g1.getPullRequests({}, function (err, data) {
+//     console.log(err || data);
+// });
+g1.getIssues({}, function (err, data) {
     console.log(err || data);
 });
