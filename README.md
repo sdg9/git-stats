8c288b55f67e2d4dd3af758fb6d97539c25c663a
Steven Goff <stevengoff@discover.com>
2017-01-28 14:33:22 -0600

    Enhanced to pull down data locally and then use it to write excel file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-TODO: Work In Progress
+Create .git-stats-config.js at your home directory (~/)
+
+Configure it to point to the environment you care about
+
+```javascript
+module.exports = {
+  "username": "myGithub username", // if using basic auth (optional)
+  "password": "my github password", // if using basic auth (optional)
+  "scheme": "basic", // if using basic auth (optional)
+  "org": "my org/user",
+  "repo": "my repo"
+};
+```
--- example/index.js
+++ example/index.js
@@ -1,16 +1,24 @@
 // Dependencies
 var GitStats = require("../lib");
+var jsonfile = require('jsonfile');
 
 // Create the GitStats instance
 var g1 = new GitStats();
 g1.initConfig();
-// g1.getPullRequests({}, function (err, data) {
-//     console.log(err || data);
+// g1.getIssues({state: 'all', since: '2017-01-10T00:00:00Z', per_page: 100}, function (err, data) {
+//   const file = './data.json'
+//
+//   jsonfile.writeFile(file, data, function (err) {
+//     console.error(err)
+//   })
+//   // g1.createWorkbook(data);
 // });
-g1.getIssues({}, function (err, data) {
-    // console.log(err || data);
-    g1.createWorkbook(data);
-});
+
+const file = './data.json'
+jsonfile.readFile(file, function(err, data) {
+  g1.createWorkbook(data);
+})
+
 // g1.createWorkbook({}, function (err, data) {
 //     console.log(err || data);
 // });
